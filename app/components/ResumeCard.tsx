5a453bf83d0d858ab8c77ee53a2ce247bea7708d
ankur0704 <sonavaneankur@gmail.com>
2025-07-27 16:46:02 +0530

    fetch real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router";
-
+import ScoreCircle from "~/components/ScoreCircle";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
 
@@ -26,7 +26,9 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                     {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
                     {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
                 </div>
-             
+                <div className="flex-shrink-0">
+                    <ScoreCircle score={feedback.overallScore} />
+                </div>
             </div>
             {resumeUrl && (
                 <div className="gradient-border animate-in fade-in duration-1000">
