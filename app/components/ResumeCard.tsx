91083c00bccc3f9558fb953d4e6fdc47283b1237
ankur0704 <sonavaneankur@gmail.com>
2025-07-27 15:43:02 +0530

    cloud storage, pdf to img conversion, key0value storage, and ai feedback also file uplaod etc.

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,18 +1,46 @@
-import {Link} from 'react-router';
+import {Link} from "react-router";
 
-function ResumeCard() {
-  return (
-    <Link to={'/resume/${resume.id}'} className='resume-card animate-in fade-in duration-1000'>ResumeCard
-    <div className='flex flex-col gap-2'>
-        <h2 className='text-black font-bold break-words '>
-            {resume.companyName}
-        </h2>
-        <h3 className='text-lg break-words text-gray-500'>
-            
-        </h3>
-    </div>
-    </Link>
-  )
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
+
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
+    return (
+        <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
+                    {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
+                </div>
+             
+            </div>
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={resumeUrl}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
+                </div>
+                )}
+        </Link>
+    )
 }
 
 export default ResumeCard
